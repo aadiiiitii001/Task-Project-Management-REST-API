12217b1222fb0eed6c5066b56bd9f9b67aa119cc
aadiiiitii001 <153495639+aadiiiitii001@users.noreply.github.com>
2025-12-20 10:46:46 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,9 @@
+from fastapi import FastAPI
+from app.api import auth, users, projects, tasks
 
+app = FastAPI(title="Task Management API")
+
+app.include_router(auth.router, prefix="/auth", tags=["Auth"])
+app.include_router(users.router, prefix="/users", tags=["Users"])
+app.include_router(projects.router, prefix="/projects", tags=["Projects"])
+app.include_router(tasks.router, prefix="/tasks", tags=["Tasks"])
