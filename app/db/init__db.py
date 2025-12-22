5801beebd3fcf56d9b833b642afab67f8bde9c71
aadiiiitii001 <153495639+aadiiiitii001@users.noreply.github.com>
2025-12-22 09:44:38 +0530

    Update init__db.py

--- app/db/init__db.py
+++ app/db/init__db.py
@@ -2,5 +2,5 @@ from app.db.base import Base
 from app.db.session import engine
 from app.models import users, project, task
 
-def init__db():
-  Base.metadata.create_all(bind=engine)
+def init_db_for_dev_only():
+    Base.metadata.create_all(bind=engine)
