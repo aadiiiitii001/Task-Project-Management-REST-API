927bedafb24d8aad79a13862097b6d02041a66a8
Aditi Nayak <153495639+aditi-nayak-ai@users.noreply.github.com>
2026-05-31 18:01:50 +0530

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -3,7 +3,6 @@ from app.db.base import Base
 
 class User(Base):
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
