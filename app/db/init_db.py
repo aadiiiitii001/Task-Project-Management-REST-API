7336a5d647286e265f8a348d76327c3f566a5d25
Aditi Nayak <153495639+aditi-nayak-ai@users.noreply.github.com>
2026-05-31 18:34:29 +0530

    Update init_db.py

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -1,6 +1,9 @@
 from app.db.base import Base
 from app.db.session import engine
-import app.db.base_class  # noqa
+import app.models.user
+import app.models.project
+import app.models.task
 
 def init_db_for_dev_only():
     Base.metadata.create_all(bind=engine)
+
